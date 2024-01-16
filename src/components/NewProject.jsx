9861dfbdfa315d1e833476a8d275c0dcc96ce7d8
naveed60 <advmiannaveedali@gmail.com>
2024-01-16 23:26:35 +0500

    new project component updated

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,9 +1,18 @@
+import Input from "./Input";
+
 export default function NewProject(){
  return(
-    <div>
-        <menu>
-            
-        </menu>
+    <div className="w-[35rem] mt-16">
+      <menu className="flex items-center justify-end gap-4 my-4">
+        <li><button className="bg-red-600  rounded-md px-4 py-2 text-white hover:text-stone-950">Cancel</button></li>
+        <li><button className="bg-blue-600  rounded-md px-4 py-2 text-white hover:text-stone-950">Save</button></li>
+      </menu>
+      <div>
+      <Input label="Title"/>
+      <Input label="Description" textarea />
+      <Input label="Due Date"/>
+
+      </div>
     </div>
  )
 }
