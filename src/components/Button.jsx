6d3d2806d65a81e156badd8241f036d987576887
naveed60 <advmiannaveedali@gmail.com>
2024-01-16 23:59:00 +0500

    button components  added

---
+++ src/components/Button.jsx
@@ -0,0 +1,8 @@
+export default function Button({children}){
+    return(
+        <button className="px-4 py-2 text-xs md:text-base rounded-md bg-green-400 text-white-400 hover:bg-blue-600 hover:text-stone-100">+Add Project 
+        {children}
+        </button>
+
+    )
+}
