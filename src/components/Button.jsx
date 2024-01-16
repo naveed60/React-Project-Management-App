18b551255073af509988d9a993704fcddc6cdcfd
naveed60 <advmiannaveedali@gmail.com>
2024-01-17 00:15:12 +0500

    button components  added

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,6 @@
-export default function Button({children}){
+export default function Button({children, ...props}){
     return(
-        <button className="px-4 py-2 text-xs md:text-base rounded-md bg-green-400 text-white-400 hover:bg-blue-600 hover:text-stone-100">+Add Project 
+        <button className="px-4 py-2 text-xs md:text-base rounded-md bg-green-400 text-white-400 hover:bg-blue-600 hover:text-stone-100" {...props}>
         {children}
         </button>
 
