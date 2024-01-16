7d594433281461d6bfaf544afb3d737ee30efbde
naveed60 <advmiannaveedali@gmail.com>
2024-01-17 00:16:04 +0500

    sidebar components  updated

--- src/components/ProjectsSidebar.jsx
+++ src/components/ProjectsSidebar.jsx
@@ -5,7 +5,8 @@ export default function ProjectSidebar() {
     <aside className=" px-8 py-16 bg-blue-900 text-stone-50 md-w-72 rounded-r-xl">
     <h1 className="mb-8 font-bold uppercase md:text-xl text-stone-200">Your Projects</h1>
       <div>
-        <Button className="px-4 py-2 text-xs md:text-base rounded-md bg-green-400 text-white-400 hover:bg-blue-600 hover:text-stone-100">+Add Project </Button>
+        <Button> +Add Project</Button>
+
       </div>
       <ul></ul>
   </aside>
